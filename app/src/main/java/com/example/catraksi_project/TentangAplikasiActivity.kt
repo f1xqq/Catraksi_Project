4cb3d8f9c3caae211e958e055f3ec1b38c972484
@syafiq20si <Syafiq Mubarak Defani>
2023-01-05 21:39:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/catraksi_project/TentangAplikasiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.catraksi_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TentangAplikasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tentang_aplikasi)
+    }
+}
